e619ce2574c60989dfa328e40dbf123f14cd59fd
Shandea <86807815+Shandea@users.noreply.github.com>
2021-09-20 14:45:23 -0700

    created login schema and post request

---
+++ models/LoginModel.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+let LoginSchema = new Schema({
+  email: {
+    type: String,
+  },
+  password: {
+      type: String,
+  },
+});
+
+const LoginModel = mongoose.model("login", LoginSchema);
+
+module.exports = LoginModel;
--- routes/login/login.js
+++ routes/login/login.js
@@ -1,38 +1,24 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-require('dotenv').config()
-// const mongoose = require('mongoose')
-app.use(cors())
+const LoginModel = require("../models/LoginModel");
 
-
-const Username = require("./login");
-
-// mongoose.connect(process.env.Mongo_URI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true });
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }))
-
-app.use((req, res, next) => {
-  console.log("method: " + req.method)
-  console.log("path: " + req.path)
-  console.log("ip: " + req.ip)
-
-  next()
-})
-
-app.use(express.static(__dirname + "/public"))
-
-app.post('/api/test/login', (req, res) => {
-  // console.log(req.body)
-  let name = new Username(req.body)
-  name.save((err, name) => {
-    if (err) {
-      res.json({
-        "message": "Username already exists"
-      })
-    } else {
-      res.json(name)
+module.exports = (app) => {
+  app.get("/api/test/login", (req, res) => {
+    res.json({
+      message: "test success",
+    });
+  });
+  app.post("/api/test/login", async (req, res) => {
+    //console.log(req.body);
+    const newLoginPost = new LoginModel({
+      test: req.body.test,
+    });
+  
+    try {
+      const NewLogin = await newLoginPost.save();
+      res.status(201).json(NewLogin);
+    } catch (err) {
+      res.status(400).json({ message: err.message });
     }
-  })
-});
+  });
+  
+  
+};
