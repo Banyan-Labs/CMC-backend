5815049a7ab4b7ad916e2c58e8fc6618c1fa18f5
Pamela Augustine <pamela.augustine10@gmail.com>
2021-09-28 06:57:10 -0700

    added GET route to find all months

--- routes/month.js
+++ routes/month.js
@@ -1,32 +1,41 @@
-const MonthlyCallModel = require ("../models/MonthlyCall")
+const MonthlyCallModel = require("../models/MonthlyCall");
 
-module.exports = (app)  => {
-// app.get("/api/months", async (req,res) => {
-//     try {
-//         const months = await Month.find()
-//         res.json(months)
-//     } catch (err) {
-//         res.status(500).json({ message: err.message})
-//     }
-// });
-app.post("/api/month", async (req, res) => {
-    //console.log(req.body);
+module.exports = (app) => {
+  //GET route to return ALL months
+app.get('/', (req, res) => {
+  try {
+    MonthlyCallModel.find({}, (err, month) => {
+      res.status(201).json(month)
+    })
+  }catch (err){
+    res.status(500).json({ message: err.message})
+  }
+
+})
+  //POST route to add new month
+  app.post("/api/month", async (req, res) => {
+   
+
     const MonthlyCall = new MonthlyCallModel({
       month: req.body.month,
-      introductions: [{
-        name: req.body.introductions.name,
-        introDescription: req.body.introductions.introDescription,
-        introImage: req.body.introductions.introImage
-      }],
-      trainings: [{
-        trainingTitle: req.body.trainings.trainingTitle,
-        presenter: req.body.trainings.presenter,
-        trainingDescription: req.body.trainings.trainingDescription,
-        role: req.body.trainings.role
-      }],
-      otherNotes: req.body.otherNotes
+      introductions: [
+        {
+          name: req.body.introductions.name,
+          introDescription: req.body.introductions.introDescription,
+          introImage: req.body.introductions.introImage,
+        },
+      ],
+      trainings: [
+        {
+          trainingTitle: req.body.trainings.trainingTitle,
+          presenter: req.body.trainings.presenter,
+          trainingDescription: req.body.trainings.trainingDescription,
+          role: req.body.trainings.role,
+        },
+      ],
+      otherNotes: req.body.otherNotes,
     });
-  
+
     try {
       const NewMonthlyCall = await MonthlyCall.save();
       res.status(201).json(NewMonthlyCall);
@@ -34,4 +43,4 @@ app.post("/api/month", async (req, res) => {
       res.status(400).json({ message: err.message });
     }
   });
-} 
+};
